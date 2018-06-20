22b612c80ccf9694e3d13036b5e76e5391a02325
Sain-R Edwards <ske@SKE-iMac.local>
2018-06-20 04:19:49 -0700

    Changed ViewController from UIVC to UITableVC, added Project 5 files, tapped into app bundle to receive startTextFile to create array for the game, modified IB to include nav. controller

---
+++ Proj5-WordScramble/Identifiers.swift
@@ -0,0 +1,11 @@
+//
+//  Identifiers.swift
+//  Proj5-WordScramble
+//
+//  Created by Sain-R Edwards on 6/20/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import Foundation
+
+let TABLE_VIEW_CELL = "Word"
--- Proj5-WordScramble/ViewController.swift
+++ Proj5-WordScramble/ViewController.swift
@@ -6,20 +6,32 @@
 //  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            } else {
+                allWords = ["silkworm"]
+            }
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
-
-
 }
 
